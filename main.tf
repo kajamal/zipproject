620e39ab468c85f3ea1195c56615502f5b6dc16c
kajamal <75475723+kajamal@users.noreply.github.com>
2025-01-22 19:27:12 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"  # Change the region if needed
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI for us-east-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
